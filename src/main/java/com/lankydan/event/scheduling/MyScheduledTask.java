4ce72d83f110edf527f96646b98e9946bc14a325
akshaychavan <akshay.chavan2709@gmail.com>
2018-08-19 16:39:01 +0530

    added test confilice

--- src/main/java/com/lankydan/event/scheduling/MyScheduledTask.java
+++ src/main/java/com/lankydan/event/scheduling/MyScheduledTask.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.lankydan.event.scheduling.annotation.MyScheduler;
@@ -13,23 +12,30 @@ import com.lankydan.event.scheduling.annotation.MyScheduler;
 public class MyScheduledTask {
 	
 	
-	private static final Logger log = LoggerFactory.getLogger(MyScheduledTask.class);
+ 	private static final Logger log = LoggerFactory.getLogger(MyScheduledTask.class);
 
 	
-	@Scheduled(cron = "0 14 19 * * ?")
-   public void doJobOld() {
+// 	@Scheduled(cron = "0 14 19 * * ?")
+//    public void doJobOld() {
 
-//		myScheduledAnnotationBeanPostProcessor.getRegistrar().getTriggerTaskList().forEach(x -> {
-//			log.info(x.toString());
-//		});
+// //		myScheduledAnnotationBeanPostProcessor.getRegistrar().getTriggerTaskList().forEach(x -> {
+// //			log.info(x.toString());
+// //		});
 
    
-   }
+//    }
 	
 	@MyScheduler
     public void doJob() {
 
 		log.info(" doJob " + new Date());
+    
+    }
+	
+		@MyScheduler
+    public void doJob1() {
+
+		log.info(" doJob new " + new Date());
     
     }
 
